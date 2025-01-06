2b10dc38b14108c12296c208848a7c5c55037aa0
iMariee <marie.incandela@epitech.eu>
2025-01-06 15:37:46 +0100

    [ADD] lines

--- header.php
+++ header.php
@@ -1,1 +1,9 @@
-<a href="">Acceuil</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example Header</title>
+</header>
+</body>
+</html>
