47a0e34953c6435afd7ac3ddcf1448ace1a3cdf2
iMariee <marie.incandela@epitech.eu>
2025-01-06 15:56:41 +0100

    [ADD] 404 & front page

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div>
+    <section>
+        <h1>Page introuvable</h1>
+</section>
+</div>
+
+<?php get_footer(); ?>
---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div>
+    <section>
+        <h1><?php echo bloginfo('name');?><h1>
+</section>
+</div>
+<?php get_footer(); ?>
