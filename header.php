63364d5c7ea541c43e6ae7c1583bdb01df97e74b
iMariee <marie.incandela@epitech.eu>
2025-01-06 15:41:48 +0100

    [ADD] changes

--- header.php
+++ header.php
@@ -1,9 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Example Header</title>
-</header>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <title>Mon site internet</title>
+</head>
+<body>
+  <header>
+    <img src="./logo.png" alt="Mon logo">
+    <ul>
+      <li><a href="">Accueil</a></li>
+      <li><a href="">Actualités</a></li>
+      <li><a href="">À propos</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+  </header>
 </body>
 </html>
