061a6cc77b1ed8b672353c2e1c371919d2a0858c
iMariee <marie.incandela@epitech.eu>
2025-01-07 10:55:43 +0100

    [MOD] try smth

--- templates/contact.php
+++ templates/contact.php
@@ -7,7 +7,7 @@ Contact html
 <?php the_field(selector:"email"); ?>
 <h2>Coordonées GPS</h2>
 <?php 
-    $coordinates = get_field( selector: '$coordinates');
+    $coordinates = get_field( selector: 'coordinates');
 ?>
 <p>Lalitutude : <?php echo ($coordinates['latitude'])?></p>
 <p>Longitude : <?php echo ($coordinates['longitude'])?></p>
