200ed0c2738f653fc2a42d2da59ef97c13d4cf70
iMariee <marie.incandela@epitech.eu>
2025-01-07 10:54:05 +0100

    [MOD] lines

--- templates/contact.php
+++ templates/contact.php
@@ -5,7 +5,11 @@
 ?>
 Contact html
 <?php the_field(selector:"email"); ?>
-
+<h2>Coordonées GPS</h2>
 <?php 
     $coordinates = get_field( selector: '$coordinates');
 ?>
+<p>Lalitutude : <?php echo ($coordinates['latitude'])?></p>
+<p>Longitude : <?php echo ($coordinates['longitude'])?></p>
+
+<?php var_dump($coordinates); ?>
